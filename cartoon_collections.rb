a6f978f761c3108023f8840728c43644820ae10c
Charles Kiel <charleskiel@yahoo.com>
2019-09-30 07:18:29 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,42 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)
+  hash = Hash.new
+  array.each_with_index { |item, index|
+    puts "#{index+1}.  -- #{item}"
+  }
+  
+  hash   #=> {"cat"=>0, "dog"=>1, "wombat"=>2}
+
+end
+
+def summon_captain_planet(array)
+   array.map {|i| "#{i.capitalize}!"}
+end
+
+def long_planeteer_calls(array)
+  t = false
+  array.each do |a|
+    t = true if a.length > 4
+  end
+  return t
+  
   # Your code here
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
+def find_the_cheese(cheese)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  result = nil
+  cheese.each do |c|
+    
+    cheese_types.each do |t|
+      puts "#{c} - #{t} \n"
+      
+      if c == t then
+        puts "#{c} - #{t} are the same!!  \n"
+        return c
+      end
+    end
+  end
+  return result
 end
