b37b349ab2677013e73f7cceb1041e6b8af67f5e
Parker Bacall <pbacall2@gmail.com>
2019-10-11 02:04:59 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,25 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index do |item, index|
+    puts  "#{index+1} #{item}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  newArray = []
+  array.collect do |call|
+    newArray.push("#{call.capitalize}!")
+  end
+   return newArray
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any? do |word|
+    word.length > 4
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(array)
+    array.find do |word|
+      word == "cheddar" || word == "gouda" || word == "camembert"
+    end
 end
